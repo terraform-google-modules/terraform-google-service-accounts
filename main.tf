ce58c48d735105391151132865c1e3e6fe0d5900
Roscoe Pyell <roscoejp@gmail.com>
2021-03-16 12:55:01 -0400

    Update main.tf
    Index doesn't take sets and sets are unordered so swapping to use the original name list. Also changed from `> x - 1` to `>= x` since it seems easier to read.

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "google_service_account" "service_accounts" {
   for_each     = local.names
   account_id   = "${local.prefix}${lower(each.value)}"
   display_name = var.display_name
-  description  = index(local.names, each.value) > length(var.descriptions) - 1 ? var.description : element(var.descriptions, index(local.names, each.value))
+  description  = index(var.names, each.value) >= length(var.descriptions) ? var.description : element(var.descriptions, index(var.names, each.value))
   project      = var.project_id
 }
 
