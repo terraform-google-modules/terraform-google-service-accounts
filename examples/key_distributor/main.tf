2f1e2cb57a9ed7e982ff0696e9c5032bad973579
Bharath KKB <bharathkrishnakb@gmail.com>
2023-12-19 11:02:24 -0600

    chore: update examples to use registry (#103)

--- examples/key_distributor/main.tf
+++ examples/key_distributor/main.tf
@@ -15,7 +15,8 @@
  */
 
 module "distributor" {
-  source = "../../modules/key-distributor"
+  source  = "terraform-google-modules/service-accounts/google//modules/key-distributor"
+  version = "~> 4.0"
 
   project_id       = var.project_id
   public_key_file  = var.public_key_file
