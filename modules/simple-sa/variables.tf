ee1516b78c7ec0c0671c1cc0a5f4cdad8c1de6a4
Bharath KKB <bharathkrishnakb@gmail.com>
2024-05-16 12:58:01 -0500

    feat: add simple SA module for managing SA and project IAM (#118)

---
+++ modules/simple-sa/variables.tf
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  type        = string
+  description = "Project id where service account will be created"
+}
+
+variable "name" {
+  type        = string
+  description = "Name of service account"
+}
+
+variable "display_name" {
+  type        = string
+  description = "Display name of the created service accounts (defaults to 'Terraform-managed service account')"
+  default     = "Terraform-managed service account"
+}
+
+variable "description" {
+  type        = string
+  description = "Default description of the created service accounts (defaults to no description)"
+  default     = ""
+}
+
+variable "project_roles" {
+  type        = list(string)
+  description = "Roles to grant the SA in specified project"
+}
